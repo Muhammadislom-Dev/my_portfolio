187cc052e8ac99c81eebf74dfbd8c06c145f6aff
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-04-07 22:39:12 +0500

    commit

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -15,8 +15,8 @@ const About = () =>{
                  alt="" />
                  </div>
                 <div className="about__list">
-                 <h3 className='about__texts'>Hello dear! I'm Mirsoli Mirsultonov and I'm 24 years old. I'm from Uzbekistan, I live in Andijan.</h3>
-                 <h3 className='about__texts'>I started frontend training independently in October 2020, and from April 2021 I took a 5-month course at PDP IT Academy. Now I have enough skills from HTML, CSS, SCSS, SASS, Tailwind CSS, Bootstrap, jQuery, JavaScript, React JS, Redux JS, Styled-components, Material UI and Ant Design technologies.</h3>
+                 <h3 className='about__texts'>Hello dear!  I'm Muhammadislom Xojimuhammedov and I'm 18 years old. I'm from Uzbekistan, I live in Andijan.</h3>
+                 <h3 className='about__texts'>I started frontend training independently in November 2021, and from April 2021 I took a 5-month course at IT TIME Academy. Now I have enough skills from HTML, CSS, SCSS, SASS, Tailwind CSS, Bootstrap,  JavaScript, React JS, Github, Material UI and Ant Design technologies.</h3>
                  <h3 className='about__texts'>I like my job and I will be very happy if I solve the given problems quickly.</h3>
                 </div>
             </div>
