ba4e33466dc05d057d7a09e91464d45e8d46d650
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-04-08 14:50:17 +0500

    initial commit

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -27,6 +27,38 @@ const Portfolio = () =>{
                     </div>
                 </div>
             </div>
+
+            <div className="section__list">
+                <div className="section-item">
+                    <h3 className='section__names'>You Tube</h3>
+                    <p className='section__text'>You can watch the movie by logging in to the  clone version  Youtube.</p>
+                    <div className="section__items">
+                        <div className="section__title">
+                            <a href="#" className="section__link">Source</a>
+                        </div>
+                         <div className="section__titles">
+                            <a href="#" className="section__links">Demo</a>
+                         </div>
+                    </div>
+                </div>
+                <img src={youtube} alt="" className="section__pic" />
+            </div>
+
+            <div className="section__list">
+                <img src={food} alt="" className="section__img" />
+                <div className="section__item">
+                    <h3 className='section__names'>Tesla auto</h3>
+                    <p className='section__text'>You can buy the car market any way you want.</p>
+                    <div className="section__items">
+                        <div className="section__title">
+                            <a href="#" className="section__link">Source</a>
+                        </div>
+                         <div className="section__titles">
+                            <a href="#" className="section__links">Demo</a>
+                         </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
