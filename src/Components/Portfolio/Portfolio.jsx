44d14dad537175fa14f4fe6cd84257210338623d
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-04-08 22:12:22 +0500

    commit

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -8,6 +8,7 @@ import food from '../../assets/img/food.jpg'
 import github from '../../assets/img/github.png'
 import link from '../../assets/img/link.png'
 import movie from '../../assets/img/movie.jpg'
+import pixer from '../../assets/img/pixer.jpg'
 
 const Portfolio = () =>{
     return(
@@ -23,7 +24,7 @@ const Portfolio = () =>{
                     <div className="section__items">
                         <div className="section__title">
                             <a href="https://github.com/muhammadislom2004" className="section__link">Source</a>
-                            <img className='section__picture' src={github} alt="" />
+                            <a href="https://github.com/muhammadislom2004"><img className='section__picture' src={github} alt="" /></a>
                         </div>
                          <div className="section__titles">
                             <a href="#" className="section__links">Demo</a>
@@ -40,7 +41,7 @@ const Portfolio = () =>{
                     <div className="section__items">
                         <div className="section__title">
                             <a href="https://github.com/muhammadislom2004" className="section__link">Source</a>
-                            <img className='section__picture' src={github} alt="" />
+                            <a href="https://github.com/muhammadislom2004"><img className='section__picture' src={github} alt="" /></a>
                         </div>
                          <div className="section__titles">
                             <a href="#" className="section__links">Demo</a>
@@ -59,7 +60,43 @@ const Portfolio = () =>{
                     <div className="section__items">
                         <div className="section__title">
                             <a href="https://github.com/muhammadislom2004" className="section__link">Source</a>
-                            <img className='section__picture' src={github} alt="" />
+                            <a href="https://github.com/muhammadislom2004"><img className='section__picture' src={github} alt="" /></a>
+                        </div>
+                         <div className="section__titles">
+                            <a href="#" className="section__links">Demo</a>
+                            <img className='section__pictures' src={link} alt="" />
+                         </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="section__list">
+                <div className="section-item">
+                    <h3 className='section__names'>Movie film</h3>
+                    <p className='section__text'>You can watch the movie by logging in to the  clone version  Youtube.</p>
+                    <div className="section__items">
+                        <div className="section__title">
+                            <a href="https://github.com/muhammadislom2004" className="section__link">Source</a>
+                            <a href="https://github.com/muhammadislom2004"><img className='section__picture' src={github} alt="" /></a>
+                        </div>
+                         <div className="section__titles">
+                            <a href="#" className="section__links">Demo</a>
+                            <img className='section__pictures' src={link} alt="" />
+                         </div>
+                    </div>
+                </div>
+                <img src={movie} alt="" className="section__pic" />
+            </div>
+
+            <div className="section__list">
+                <img src={pixer} alt="" className="section__img" />
+                <div className="section__item">
+                    <h3 className='section__names'>Pixer</h3>
+                    <p className='section__text'>You can buy the car market any way you want.</p>
+                    <div className="section__items">
+                        <div className="section__title">
+                            <a href="https://github.com/muhammadislom2004" className="section__link">Source</a>
+                            <a href="https://github.com/muhammadislom2004"><img className='section__picture' src={github} alt="" /></a>
                         </div>
                          <div className="section__titles">
                             <a href="#" className="section__links">Demo</a>
